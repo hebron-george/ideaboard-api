3b43ae104718a2098bb33fca2b743d0811a34039
Hebron George <hebrontgeorge@gmail.com>
2018-10-21 04:32:09 -0500

    init commit

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+ideas = Idea.create([
+  {
+    title: 'Idea 1',
+    body:  'This is my first idea!'
+  },
+  {
+    title: 'Idea 2',
+    body:  'My second idea..'
+  },
+  {
+    title: '3D Farms',
+    body:  'Farms should grow vertically!'
+  },
+  {
+    title: 'Scaling cities',
+    body:  'Cities should be made like legos'
+  }
+                    ])
